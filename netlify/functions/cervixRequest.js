a39a369c8909ecf255d97db160217f00671b5476
Emma Hager <62039090+uchusei@users.noreply.github.com>
2023-10-18 20:39:47 +0200

    Update cervixRequest.js

--- netlify/functions/cervixRequest.js
+++ netlify/functions/cervixRequest.js
@@ -1,16 +1,24 @@
 const API_LIB = require(process.env.API_LIBRARY);
 
+// Function that handles serverless calls
 exports.handler = async function(event, context) {
-  const engine = new API_LIB(process.env.OPENAI_API_KEY);
   
+  // Creates a new instance of the API library with API key
+  const engine = new API_LIB(process.env.CERVIX_API_KEY);
+  
+  // Retrieves the users input from the query string
   const userInput = event.queryStringParameters.text;
   
+  // Concatenates the content from the two environment variables into a complete string
+  const fullContent = process.env.CONTENT_PART1 + process.env.CONTENT_PART2;
+
+  // Creates an API call to model with the specified parameters
   const response = await engine.chat.completions.create({
     model: process.env.MODEL_NAME,
     messages: [
       {
         "role": process.env.ROLE,
-        "content": process.env.CONTENT
+        "content": fullContent
       },
       {
         "role": "user",
@@ -24,6 +32,7 @@ exports.handler = async function(event, context) {
     presence_penalty: process.env.PRESENCE_PENALTY,
   });
   
+  // Returns response to the client
   return {
     statusCode: 200,
     body: JSON.stringify({result: response.choices[0].text.trim()}),
