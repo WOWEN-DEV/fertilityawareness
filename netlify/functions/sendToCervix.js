f9f47940439bec760dd4cf8e1ce9e93a1f8dc46c
Emma Hager <62039090+uchusei@users.noreply.github.com>
2023-10-24 02:32:34 +0200

    Update sendToCervix.js

--- netlify/functions/sendToCervix.js
+++ netlify/functions/sendToCervix.js
@@ -2,15 +2,20 @@ const axios = require('axios');
 
 exports.handler = async function(event, context) {
   const { messageHtml, isBot } = JSON.parse(event.body);
-  
-  console.log(`Sending the following data to Google Sheets: messageHtml=${messageHtml}, isBot=${isBot}`);
 
-  // Send en POST request to Google Sheet web app
-  try {
-    const response = await axios.post('https://script.google.com/macros/s/AKfycbwy9IuEv1efzuZMOWEzMMUeRDgqIZCe6fjWt4C_VaDsQRXgEsQjUvui43rcLzJGmfw/exec', { messageHtml, isBot });
-    console.log('Successfully sent data to Google Sheets:', response.data);
-  } catch (error) {
-    console.error('Failed to send data to Google Sheets:', error);
+  // Check if the messageHtml contains 'id="thinking-indicator"'
+  if (messageHtml.indexOf('id="thinking-indicator"') === -1) {
+    console.log(`Sending the following data to Google Sheets: messageHtml=${messageHtml}, isBot=${isBot}`);
+
+    // Send a POST request to the Google Sheets web app
+    try {
+      const response = await axios.post('https://script.google.com/macros/s/AKfycbwy9IuEv1efzuZMOWEzMMUeRDgqIZCe6fjWt4C_VaDsQRXgEsQjUvui43rcLzJGmfw/exec', { messageHtml, isBot });
+      console.log('Successfully sent data to Google Sheets:', response.data);
+    } catch (error) {
+      console.error('Failed to send data to Google Sheets:', error);
+    }
+  } else {
+    console.log('Message contains id="thinking-indicator". Skipping sending to Google Sheets.');
   }
 
   return {
